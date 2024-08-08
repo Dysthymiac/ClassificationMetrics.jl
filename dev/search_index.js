b3cabdb6a1ef134ec3059d088478a13aed12bbbe
Documenter.jl <documenter@juliadocs.github.io>
2024-08-08 13:08:13 +0000

    build based on 61a11c2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ClassificationMetrics","category":"page"},{"location":"#ClassificationMetrics","page":"Home","title":"ClassificationMetrics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ClassificationMetrics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ClassificationMetrics]","category":"page"}]
+}
